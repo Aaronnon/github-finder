86626ccf404593a2200356c43cfb1e3816f602e2
Aaron <fong_lun@live.com>
2021-12-23 15:44:38 -0500

    fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,11 +6,11 @@ function About() {
                 A React app to search GitHub profiles and see profile details.
             </p>
             <p className='text-lg text-gray-400'>
-                Version <span className='text-white'>1.0.0</span>
+                Version <span className='alert-text text-white'>1.0.0</span>
             </p>
             <p className='text-lg text-gray-400'>
                 Layout By:
-                <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+                <a className='alert-text text-white' href='https://twitter.com/hassibmoddasser'>
                     Hassib Moddasser
                 </a>
             </p>
